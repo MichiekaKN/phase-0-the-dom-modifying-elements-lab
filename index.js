3a782e796e4b1c07de0e3e6d02656e3e13bb6888
Kelvin Michieka <96nyakundikelvin@gmail.com>
2022-06-05 21:26:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const testVar = "hi"
+
+/*removing the node element  */
+document.querySelector("main#main").remove();
+
+/* new element assigned to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Kelvin Michieka is the champion!";
+newHeader.className = "victory";
